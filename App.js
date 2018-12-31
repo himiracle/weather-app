bd8d6a4f41f8fdffdfe94328e32b6a351698e168
blockchain1124 <blockchain1124@gmail.com>
2019-01-01 01:13:19 +0900

    1

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Constants } from 'expo';
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Image, ActivityIndicator } from "react-native";
+import { Constants } from "expo";
 
 // You can import from local files
-import AssetExample from './components/AssetExample';
+import AssetExample from "./components/AssetExample";
 
 // or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
+import { Card } from "react-native-paper";
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    isLoading: false
+  };
   render() {
+    const { isLoading } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          TEST test TEST
-        </Text>
-        <Card>
-          <AssetExample />
-        </Card>
+        {isLoading
+          ? null
+          : <View style={styles.loading}>
+              <Text>Get Weather</Text>
+            </View>}
       </View>
     );
   }
@@ -26,15 +29,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    backgroundColor: "#ecf0f1",
+    padding: 8
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+  loading: {
+    backgroundColor: "#fdf6aa"
+  }
 });
